03409d091679994a330f6951aa0e8a45025a96aa
Peter Salama <salamap@gmail.com>
2013-07-10 02:25:15 +0000

    Added file buffer toSting

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
+var fs = require('fs');
+
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var buf = new Buffer(fs.readFileSync(index.html));
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(buf.toString());
 });
 
 var port = process.env.PORT || 5000;
