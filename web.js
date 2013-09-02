685052eaa6ed889b606bb1f54e13a717b54023c5
Peter Salama <salamap@gmail.com>
2013-09-02 04:19:39 +0000

    Edit web.js

--- web.js
+++ web.js
@@ -9,6 +9,11 @@ app.get('/', function(request, response) {
   response.send(html);
 });
 
+app.get('/about', function(request, response) {
+  var html = fs.readFileSync(screen-c3130a1371.html).toString();
+  response.send(html);
+});
+
 var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
